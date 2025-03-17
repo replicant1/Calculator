b05c79582c53afc07333496dca5af2d0269b9a99
rodney.bailey <rodney.bailey@humm-group.co>
2025-03-17 13:47:20 +1100

    More mockk tests

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -6,6 +6,37 @@ class Calculator() {
     fun mem() : Int { return -1 }
 }
 
+class ThingOne {
+    fun doSomethingOne() {
+        println("One")
+    }
+    fun doSomethingTwo() {
+        println("Two")
+    }
+}
+
+class ThingTwo {
+    fun doSomethingOneTwice(thingOne: ThingOne) {
+        thingOne.doSomethingOne()
+        thingOne.doSomethingOne()
+    }
+
+    fun doSomethingOneThenTwo(thingOne: ThingOne) {
+        thingOne.doSomethingOne()
+        thingOne.doSomethingTwo()
+    }
+}
+
+interface CallMe {
+    fun callMe(str: String)
+}
+
+class Holder(val callMe: CallMe) {
+    fun call(str : String) {
+        callMe.callMe(str)
+    }
+}
+
 //class Operators {
 //    fun add(m: Int, n: Int): Int = m + n
 //    fun subtract(n: Int, m: Int): Int = n - m
--- app/src/test/java/com/example/calculator/CalculatorTest.kt
+++ app/src/test/java/com/example/calculator/CalculatorTest.kt
@@ -1,18 +1,19 @@
 package com.example.calculator
 
+import io.mockk.Called
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.junit4.MockKRule
+import io.mockk.justRun
 import io.mockk.mockk
+import io.mockk.slot
+import io.mockk.spyk
 import io.mockk.verify
-import org.junit.Assert
+import io.mockk.verifyOrder
 import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
-import org.junit.Rule
-import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -21,7 +22,7 @@ import org.junit.runner.RunWith
  */
 class CalculatorTest {
     // This SUT is *not* mocked
-    lateinit var calc : Calculator
+    lateinit var calc: Calculator
 
     // This SUT is a mock is created by MockK
     @MockK
@@ -37,7 +38,7 @@ class CalculatorTest {
 
     @Test
     fun test_mockk_creates_mock_with_no_dependencies() {
-        every { mockedCalculator.add(any(), any())} returns 10
+        every { mockedCalculator.add(any(), any()) } returns 10
         val result = mockedCalculator.add(3, 4)
         assertEquals(10, result)
     }
@@ -50,7 +51,7 @@ class CalculatorTest {
 
     @Test
     fun test_every_mem() {
-        every { mockedCalculator.mem()} returns 5
+        every { mockedCalculator.mem() } returns 5
         val result = mockedCalculator.mem()
         assertEquals(5, result)
     }
@@ -59,7 +60,7 @@ class CalculatorTest {
     fun local_mock_calculator() {
         // Can't use @MockK annotation
         val localMockCalculator = mockk<Calculator>()
-        every { localMockCalculator.mem()} returns 5
+        every { localMockCalculator.mem() } returns 5
         val result = localMockCalculator.mem()
         assertEquals(5, result)
     }
@@ -67,12 +68,97 @@ class CalculatorTest {
     @Test
     fun test_one_wildcard_argument() {
         every { mockedCalculator.add(5, any()) } returns 10
-        every { mockedCalculator.add(6, any())} returns 16
+        every { mockedCalculator.add(6, any()) } returns 16
 
         assertEquals(10, mockedCalculator.add(5, 6))
         assertEquals(10, mockedCalculator.add(5, 7))
 
         assertEquals(16, mockedCalculator.add(6, 1))
-        assertEquals(16, mockedCalculator.add(6,8))
+        assertEquals(16, mockedCalculator.add(6, 8))
+    }
+
+    @Test
+    fun test_verify_direct_call() {
+        val thingOne = mockk<ThingOne>()
+        every { thingOne.doSomethingOne() } answers { }
+        thingOne.doSomethingOne()
+        verify { thingOne.doSomethingOne() }
+    }
+
+    @Test
+    fun test_verify_just_run() {
+        val thingOne = mockk<ThingOne>()
+        val thingTwo = ThingTwo()
+        justRun { thingOne.doSomethingOne() } // Alt to "answers {}" when there is no return value
+        thingTwo.doSomethingOneTwice(thingOne)
+        verify { thingOne.doSomethingOne() }
+    }
+
+    @Test
+    fun test_verify_indirect_call() {
+        val thingOne = mockk<ThingOne>()
+        val thingTwo = ThingTwo()
+        every { thingOne.doSomethingOne() } answers {}
+        thingTwo.doSomethingOneTwice(thingOne)
+        verify { thingOne.doSomethingOne() }
+    }
+
+    @Test
+    fun test_spy_avoids_no_answer_error() {
+        val thingOne = spyk<ThingOne>()
+        val thingTwo = spyk<ThingTwo>()
+        thingTwo.doSomethingOneTwice(thingOne)
+        verify { thingOne.doSomethingOne() }
+    }
+
+    @Test
+    fun test_verify_exactly() {
+        val thingOne = mockk<ThingOne>()
+        val thingTwo = ThingTwo()
+        justRun { thingOne.doSomethingOne() }
+        thingTwo.doSomethingOneTwice(thingOne)
+        verify(exactly = 2) { thingOne.doSomethingOne() }
+        verify(inverse = true) { thingOne.doSomethingTwo() }
+    }
+
+    @Test
+    fun test_verify_order() {
+        val thingOne = spyk<ThingOne>()
+        val thingTwo = spyk<ThingTwo>()
+        thingTwo.doSomethingOneThenTwo(thingOne)
+        verifyOrder {
+            thingOne.doSomethingOne()
+            thingOne.doSomethingTwo()
+        }
+    }
+
+    @Test
+    fun test_slots_added() {
+        val slot1 = slot<Int>()
+        every {
+            mockedCalculator.add(
+                capture(slot1),
+                any()
+            )
+        } returns 600
+        assertEquals(600, mockedCalculator.add(2, 3))
+        assertTrue(slot1.isCaptured)
+        assertEquals(2, slot1.captured)
+    }
+
+    @Test
+    fun test_is_called() {
+        val called = spyk<CallMe>()
+        val holder = Holder(called)
+        holder.call("Bob")
+        verify { called.callMe(any()) }
+    }
+
+    @Test
+    fun test_more_less() {
+        every { mockedCalculator.add(more(5), more(5)) } returns 1
+        every { mockedCalculator.add(less(5), less(5)) } returns 2
+        assertEquals(1, mockedCalculator.add(66, 67))
+        assertEquals(2, mockedCalculator.add(2, 3))
     }
 }
